ddb197f431100e848c0054e2280ac0c4f04b8123
yuneshwaran <yunesh2403@gmail.com>
2023-04-30 08:49:52 +0530

    keys upsate

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,5 @@
-dbPassword='mongodb+srv://travel-app:yato.123@travel-app.fsmrpjt.mongodb.net/travel?retryWrites=true&w=majority'
+
+dbPassword='mongodb+srv://travel-app:yato.123@travel-app.fsmrpjt.mongodb.net/ares?retryWrites=true&w=majority'
 
 module.exports = {
   mongoURI: dbPassword
