79de9b1f7de62e43b5354958bf9755631e348709
yuneshwaran <yunesh2403@gmail.com>
2023-05-19 16:11:02 +0530

    databae

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,4 @@
-dbPassword='mongodb+srv://yato:yat0123@cluster0.xi6csuw.mongodb.net/ares?retryWrites=true&w=majority'
+dbPassword='mongodb+srv://yato:yato123@cluster0.xi6csuw.mongodb.net/ares?retryWrites=true&w=majority'
 
 module.exports = {
   mongoURI: dbPassword
